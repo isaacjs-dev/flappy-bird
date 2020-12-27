72775b9976bb4527a84eef0c900ae471ee58b3da
Isaac Silvério <isaacjs.lab@gmail.com>
2020-12-27 08:44:50 -0300

    Criando os canos e a lógica de colisão

--- jogo.js
+++ jogo.js
@@ -8,7 +8,7 @@ const soundHIT = new Audio();
 soundHIT.src = './sound/hit.wav';
 
 const globals = {
-
+    frames: 0,
 }
 
 const flappyBird = () => ({
@@ -23,11 +23,28 @@ const flappyBird = () => ({
     gravity: 0.25,
     speed: 0,
     skip: 4.6,
+    currentFrame: 0,
+    movement: [
+        { sourceX: 0, sourceY: 0 },
+        { sourceX: 0, sourceY: 26 },
+        { sourceX: 0, sourceY: 52 }
+    ],
+    setFrameCurrent() {
+        const frameInterval = 10
+        const passedBreak = globals.frames % frameInterval === 0;
+
+        if (passedBreak) {
+            const incrementBase = 1;
+            const increment = incrementBase + this.currentFrame;
+            const numOfRepet = this.movement.length;
+            this.currentFrame = increment % numOfRepet
+        }
+    },
     jump() {
         this.speed = - this.skip;
     },
     update() {
-        if (collided(globals.flappyBird, floor)) {
+        if (collided(globals.flappyBird, globals.floor)) {
             soundHIT.play()
             setTimeout(() => {
                 screens.screenRoute(screens.HOME);
@@ -38,9 +55,11 @@ const flappyBird = () => ({
         this.drawY = this.drawY + this.speed;
     },
     draw() {
+        this.setFrameCurrent();
+        const { sourceX, sourceY } = this.movement[this.currentFrame]
         contexto.drawImage(
             sprites,
-            this.sourceX, this.sourceY,
+            sourceX, sourceY,
             this.sourceWidth, this.sourceHeight,
             this.drawX, this.drawY,
             this.drawWidth, this.drawHeight
@@ -48,7 +67,97 @@ const flappyBird = () => ({
     }
 })
 
-const floor = {
+const createTubes = () => ({
+    sourceWidth: 52,
+    sourceHeight: 400,
+    drawWidth: 52,
+    drawHeight: 400,
+    pairOfTubes: [],
+
+    CollidedBird(pairOfTube) {
+        const birdTop = globals.flappyBird.drawY;
+        const birdBottom = globals.flappyBird.drawY + globals.flappyBird.drawHeight;
+
+        if (globals.flappyBird.drawX >= pairOfTube.drawX) {
+            if (birdTop <= pairOfTube.top.drawY + this.sourceHeight) {
+                return true;
+            }
+
+            if (birdBottom >= pairOfTube.bottom.drawY) {
+                return true;
+            }
+
+
+        }
+        return false;
+    },
+
+    update() {
+        drawNewtudo = globals.frames % 100 === 0;
+
+        if (drawNewtudo) {
+            this.pairOfTubes.push({
+                drawX: canvas.width,
+                drawY: -150 * (Math.random() + 1),
+            })
+        }
+
+        this.pairOfTubes.forEach((pairOfTube) => {
+            pairOfTube.drawX = pairOfTube.drawX - 2;
+
+            if (this.CollidedBird(pairOfTube)) {
+                screens.screenRoute(screens.HOME);
+            }
+
+            if (pairOfTube.drawX + this.sourceWidth <= -1) {
+                this.pairOfTubes.shift();
+            }
+        }
+
+        )
+
+    },
+
+    draw() {
+        this.pairOfTubes.forEach((pairOfTube) => {
+            const spaceTubesBetween = 90;
+            const drawYRondom = pairOfTube.drawY;
+
+            pairOfTube.top = {
+                sourceX: 52,
+                sourceY: 169,
+                drawX: pairOfTube.drawX,
+                drawY: drawYRondom,
+            }
+
+            pairOfTube.bottom = {
+                sourceX: 0,
+                sourceY: 169,
+                drawX: pairOfTube.drawX,
+                drawY: this.sourceHeight + spaceTubesBetween + drawYRondom,
+            }
+
+
+            contexto.drawImage(
+                sprites,
+                pairOfTube.top.sourceX, pairOfTube.top.sourceY,
+                this.sourceWidth, this.sourceHeight,
+                pairOfTube.top.drawX, pairOfTube.top.drawY,
+                this.drawWidth, this.drawHeight
+            )
+
+            contexto.drawImage(
+                sprites,
+                pairOfTube.bottom.sourceX, pairOfTube.bottom.sourceY,
+                this.sourceWidth, this.sourceHeight,
+                pairOfTube.bottom.drawX, pairOfTube.bottom.drawY,
+                this.drawWidth, this.drawHeight
+            )
+        })
+    }
+})
+
+const gameFloor = () => ({
     sourceX: 0,
     sourceY: 610,
     sourceWidth: 224,
@@ -57,6 +166,13 @@ const floor = {
     drawY: canvas.height - 112,
     drawWidth: 224,
     drawHeight: 112,
+    update() {
+        const moveFloor = 1;
+        const repeatIn = this.sourceWidth / 2;
+        const moved = this.drawX - moveFloor;
+
+        this.drawX = moved % repeatIn;
+    },
     draw() {
         contexto.drawImage(
             sprites,
@@ -74,7 +190,9 @@ const floor = {
             this.drawWidth, this.drawHeight
         )
     }
-}
+})
+
+
 
 const bgGamer = {
     sourceX: 390,
@@ -137,13 +255,18 @@ const screens = {
     HOME: {
         init() {
             globals.flappyBird = flappyBird();
+            globals.floor = gameFloor();
+            globals.tubes = createTubes();
+
         },
         draw() {
-            screens.GAME.draw()
-            mensagemGetReady.draw()
+            bgGamer.draw();
+            globals.flappyBird.draw();
+            globals.floor.draw();
+            mensagemGetReady.draw();
         },
         update() {
-
+            globals.floor.update();
         },
         click() {
             screens.screenRoute(screens.GAME);
@@ -152,11 +275,14 @@ const screens = {
     GAME: {
         draw() {
             bgGamer.draw();
-            floor.draw();
             globals.flappyBird.draw();
+            globals.tubes.draw();
+            globals.floor.draw();
         },
         update() {
             globals.flappyBird.update();
+            globals.floor.update();
+            globals.tubes.update();
         },
         click() {
             globals.flappyBird.jump();
@@ -174,6 +300,8 @@ const collided = (flappyBird, gameFloor) => flappyBird.drawY + flappyBird.source
 function loop() {
     screens.ative.draw();
     screens.ative.update();
+
+    globals.frames = globals.frames + 1;
     requestAnimationFrame(loop);
 }
 
